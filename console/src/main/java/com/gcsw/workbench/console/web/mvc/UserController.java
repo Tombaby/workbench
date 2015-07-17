5d52e163c0362dc3c863558dae4e436db8a20272
Tombaby <shayne1978@gmail.com>
2015-07-17 18:54:39 +0800

    add controller

---
+++ console/src/main/java/com/gcsw/workbench/console/web/mvc/UserController.java
@@ -0,0 +1,8 @@
+package com.gcsw.workbench.console.web.mvc;
+
+/**
+ * Created by Administrator on 2015/7/17.
+ */
+public class UserController {
+
+}
---
+++ console/src/main/java/com/gcsw/workbench/console/web/restful/UserController.java
@@ -0,0 +1,7 @@
+package com.gcsw.workbench.console.web.restful;
+
+/**
+ * Created by Administrator on 2015/7/17.
+ */
+public class UserController {
+}
